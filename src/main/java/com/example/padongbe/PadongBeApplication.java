c856cae32db7baf15bb56578cbaa4dd9dc180f9b
yeilkk <yeil99@naver.com>
2025-04-09 15:55:53 +0900

    set: 프로젝트 세팅

---
+++ src/main/java/com/example/padongbe/PadongBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.padongbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadongBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadongBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/padongbe/PadongBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.padongbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadongBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
